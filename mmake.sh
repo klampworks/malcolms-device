89cd394ddac3c30cf1a789e3fada74385a29ba74
yukipyon <wolfvbear@gmail.com>
2013-12-12 19:17:08 +0000

    Updated directory information.

--- mmake.sh
+++ mmake.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-make -C ../linux-3.2.51/ M=/home/debian/ modules CFLAGS="-E -g -O2"
-rmmod yes && insmod ../yes.ko
+make -C ../linux-3.2.51/ M=/home/debian/malcolms-device/ modules CFLAGS="-E -g -O2"
+rmmod yes 
+insmod yes.ko
