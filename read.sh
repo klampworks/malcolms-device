fc80debd93ff9a7697767089bd08f5f51161778d
yukipyon <wolfvbear@gmail.com>
2013-12-14 10:48:35 +0000

    Added argument for device name.

--- read.sh
+++ read.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+if [ -z "$1" ]
+then
+	dev="yes"
+else
+	dev="$1"	
+fi
+
+if [ -z "$2" ]
+then
+	num="12"
+else
+	num="$2"	
+fi
+
 > out
-dd if=/dev/yes of=out count=$1
+dd if=/dev/$dev of=out count=$num
 echo
 cat out
 echo
